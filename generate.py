0d3b7a0114ee5fd09ffd19386da909a9651fcd12
Zaccaria <146808411+ZaccariaElka@users.noreply.github.com>
2024-09-12 11:23:26 +0200

    Update generate.py

--- generate.py
+++ generate.py
@@ -1,6 +1,7 @@
 import random
 import hashlib as hs
 import hmac
+from ecdsa import SigningKey, SECP256k1
 
 bip39_list = open("words.txt", "r").read().split("\n") #split it and transform it into an array
 
@@ -13,7 +14,7 @@ while len(words_list) < 12:
     if random_word not in words_list: #check if the word is already in the array
         words_list.append(random_word)
 
-mnemonic_code = ' '.join(words_list)
+mnemonic_code = ' '.join(words_list).strip()
 
 print(f"Mnemonic phrase: {mnemonic_code}") #DISPLAY
 
@@ -32,7 +33,15 @@ hmac_result = hmac.new(key, seed, hs.sha512).digest() #get digest bytes of the c
 master_private_key = hmac_result[:32] #get the master private key (the first 32 bytes)
 chain_code = hmac_result[32:] #get the chain code in case i will need child keys (the last 32 bytes)
 
-print(f"Private Key (hex): {master_private_key.hex()}") #DISPLAY
-print(f"Chain Code (hex): {chain_code.hex()}") #DISPLAY
+print("---")
+print(f"Master Private Key: {master_private_key.hex()}") #DISPLAY
+print(f"Chain Code: {chain_code.hex()}") #DISPLAY
+
+public_key = SigningKey.from_string(master_private_key, curve=SECP256k1).verifying_key
+public_key_hex = public_key.to_string().hex()
+
+print("---")
+print(f"Public Key (hex): {public_key_hex}") #DISPLAY
+
 
 #NOT FINISHED
